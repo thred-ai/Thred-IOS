f1c6bff9b0886927e6749b5e24bbdcd18789d0a6
Artakoroushnia <arta2001@icloud.com>
2019-12-25 20:37:14 -0500

    Initial Commit

---
+++ Thred/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Thred
+//
+//  Created by Arta Koroushnia on 2019-12-25.
+//  Copyright © 2019 Thred. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
